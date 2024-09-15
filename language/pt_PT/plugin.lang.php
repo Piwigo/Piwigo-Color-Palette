e2ae11a96baa682b27b953838b160413b58625b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-15 20:13:36 +0200

    Update pt_PT, thanks to ANO

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sample image coverage from center (default 100% - full image)'] = 'Exemplo de imagem coberta do centro (padrão 100% - imagem completa)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'Imagem de cobertura deve ter um valor entre %d e %d';
+$lang['Sample image must have a size between %d and %d'] = 'Imagem deve ter tamanho entre  %d e %d';
+$lang['Sample image size for palette generation (default %d px)'] = 'Tamanho da imagem de amostra para geração de paleta (padrão %d px)';
+$lang['Clear all palettes'] = 'Limpar todas as paletes';
+$lang['Clear color palette'] = 'Limpar a palete de cores';
+$lang['Color Palette'] = 'Palete de cor';
+$lang['ColorPalette'] = 'Palete de cor';
+$lang['Configuration successfully updated'] = 'Configuração foi atualizada com sucesso';
+$lang['Generate color palette'] = 'Gerar palete de cor';
+$lang['Generate palette on image page'] = 'Gerar paleten na pagina da imagem';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'Se ainda não foi gerada palete para a imagem, esta será efetuada quando a imagem for aberta pela primeira vez.';
+$lang['Number of colors'] = 'Número de cores';
+$lang['Number of colors in palette (default %d)'] = 'Numero de cores na palete: (default %d). ';
+$lang['Number of colors must have a value between %d and %d'] = 'Número de cores deve ter um valor entre  %d e %d';
+$lang['Operation successfully completed'] = 'Operação foi completa com sucesso';
+$lang['Palette'] = 'Palete';
+$lang['Palette Search'] = 'Procurar Palete';
+$lang['Sample image coverage'] = 'Exemplo de imagem';
+$lang['Sample image size'] = 'Exemplo do tamanho de imagem';
+$lang['Unable to process %s'] = 'Não processado  %s';
+$lang['With color palette'] = 'Com cor de palete';
+$lang['Without color palette'] = 'Sem cor de palete';
