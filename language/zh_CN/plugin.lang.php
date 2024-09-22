f077b30ed6b6c55a750a9bda28c70344f4204837
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-22 07:22:50 +0200

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Clear color palette'] = '清除调色板';
+$lang['Color Palette'] = '调色板';
+$lang['ColorPalette'] = '调色板';
+$lang['Configuration successfully updated'] = '配置已成功更新';
+$lang['Generate color palette'] = '生成调色板';
+$lang['Number of colors must have a value between %d and %d'] = '颜色数必须介于 %d 和 %d 之间';
+$lang['Operation successfully completed'] = '操作已成功完成';
+$lang['Palette'] = '调色板';
+$lang['Palette Search'] = '调色板搜索';
+$lang['Sample image coverage must have a value between %d and %d'] = '样本图像覆盖率必须介于 %d 和 %d 之间';
+$lang['Sample image must have a size between %d and %d'] = '样本图像的尺寸必须介于 %d 和 %d 之间';
+$lang['Unable to process %s'] = '无法处理 %s';
+$lang['With color palette'] = '带调色板';
+$lang['Without color palette'] = '不带调色板';
+$lang['Clear all palettes'] = '清除所有调色板';
+$lang['Generate palette on image page'] = '在图像页面上生成调色板';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = '如果尚未为图像生成调色板，则将在第一次打开图像时完成此操作';
+$lang['Number of colors'] = '颜色数量';
+$lang['Number of colors in palette (default %d)'] = '调色板中的颜色数量（默认 %d）';
+$lang['Sample image coverage'] = '样本图像覆盖范围';
+$lang['Sample image coverage from center (default 100% - full image)'] = '从中心开始的样本图像覆盖范围（默认 100% - 完整图像）';
+$lang['Sample image size'] = '样本图像大小';
+$lang['Sample image size for palette generation (default %d px)'] = '调色板生成的样本图像大小（默认 %d 像素）';
