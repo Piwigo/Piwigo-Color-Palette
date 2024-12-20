46326415d12649e651b272276dc2a2ed19a1f410
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:31:29 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unable to process %s'] = 'לא ניתן לעבד %s';
+$lang['With color palette'] = 'עם לוח צבעים';
+$lang['Without color palette'] = 'בלי לוח צבעים';
+$lang['Clear all palettes'] = 'לנקות את כל הפלטים';
+$lang['Clear color palette'] = 'לנקות את פלט הצבע';
+$lang['Color Palette'] = 'פלט צבע';
+$lang['ColorPalette'] = 'פלט צבע';
+$lang['Configuration successfully updated'] = 'התצורה עודכנה בהצלחה';
+$lang['Generate color palette'] = 'ליצור פלט צבע';
+$lang['Generate palette on image page'] = 'ליצור פלט בעמוד התמונה';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = 'אם פלט עדיין לא נוצר עבור התמונה, זה יתבצע בפעם הראשונה שהתמונה נפתחת';
+$lang['Number of colors'] = 'מספר הצבעים';
+$lang['Number of colors in palette (default %d)'] = 'מספר הצבעים בפלט (ברירת מחדל %d)';
+$lang['Number of colors must have a value between %d and %d'] = 'מספר הצבעים חייב להיות בין %d ל-%d';
+$lang['Operation successfully completed'] = 'הפעולה הושלמה בהצלחה';
+$lang['Palette'] = 'פלט';
+$lang['Palette Search'] = 'חיפוש בפלט';
+$lang['Sample image coverage'] = 'כיסוי תמונת דוגמה';
+$lang['Sample image coverage from center (default 100% - full image)'] = 'כיסוי תמונת דוגמה מהמרכז (ברירת מחדל 100% - תמונה מלאה)';
+$lang['Sample image coverage must have a value between %d and %d'] = 'כיסוי תמונת דוגמה חייב להיות בין %d ל-%d';
+$lang['Sample image must have a size between %d and %d'] = 'תמונת דוגמה חייבת להיות בגודל בין %d ל-%d';
+$lang['Sample image size'] = 'גודל תמונת דוגמה';
+$lang['Sample image size for palette generation (default %d px)'] = 'גודל תמונת דוגמה ליצירת פלט (ברירת מחדל %d פיקסלים)';
