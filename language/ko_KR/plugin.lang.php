548e5080266bc4b037d463c87cce6f48109b7ce7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-08 08:14:14 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Clear color palette'] = '컬러 팔레트 지우기';
+$lang['Color Palette'] = '컬러 팔레트';
+$lang['ColorPalette'] = '컬러 팔레트';
+$lang['Generate color palette'] = '컬러 팔레트 생성';
+$lang['Unable to process %s'] = '%s(을)를 처리할 수 없습니다';
+$lang['With color palette'] = '컬러 팔레트 사용';
+$lang['Without color palette'] = '컬러 팔레트 미사용';
+$lang['Clear all palettes'] = '모든 팔레트 지우기';
+$lang['Configuration successfully updated'] = '설정이 성공적으로 업데이트되었습니다';
+$lang['Generate palette on image page'] = '이미지 페이지에서 팔레트 생성';
+$lang['If palette is not yet generated for the image, this will be done the first time the image is opened'] = '이미지에 대해 팔레트가 아직 생성되지 않은 경우, 이미지를 처음 열 때 생성됩니다';
+$lang['Number of colors'] = '색상 수';
+$lang['Number of colors in palette (default %d)'] = '팔레트의 색상 수 (기본값 %d)';
+$lang['Number of colors must have a value between %d and %d'] = '색상 수는 %d와 %d 사이의 값이어야 합니다';
+$lang['Operation successfully completed'] = '작업이 성공적으로 완료되었습니다';
+$lang['Palette'] = '팔레트';
+$lang['Palette Search'] = '팔레트 검색';
+$lang['Sample image coverage'] = '샘플 이미지 커버리지';
+$lang['Sample image coverage from center (default 100% - full image)'] = '중앙 기준 샘플 이미지 커버리지 (기본값 100% - 전체 이미지)';
+$lang['Sample image coverage must have a value between %d and %d'] = '샘플 이미지 커버리지는 %d와 %d 사이의 값이어야 합니다';
+$lang['Sample image must have a size between %d and %d'] = '샘플 이미지는 %d와 %d 사이의 크기를 가져야 합니다';
+$lang['Sample image size'] = '샘플 이미지 크기';
+$lang['Sample image size for palette generation (default %d px)'] = '팔레트 생성을 위한 샘플 이미지 크기 (기본값 %d px)';
